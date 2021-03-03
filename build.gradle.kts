3c13145045d989e9c33b7c309cfe18d4ff1f4597
Articdive <13535885+Articdive@users.noreply.github.com>
2021-03-03 18:51:30 +0100

    Also run the deobfuscator for the implemented version when specifying a non-implemented version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,13 @@ tasks {
         val project : Project = try {
             project(":DataGenerator:$version")
         } catch (e : UnknownProjectException ) {
-            // TODO: Get closest version and not latest version
-            project(":DataGenerator:1.16.5")
+            // TODO: Get closest version and not use a hardfixed version
+            val closestVersion = "1.16.5"
+            // Run data generator for the closestVersion (important as it used to compile!)
+            dependsOn(project(":Deobfuscator").tasks.getByName<JavaExec>("run") {
+                setArgsString(closestVersion)
+            })
+            project(":DataGenerator:$closestVersion")
         }
         dependsOn(project.tasks.getByName<JavaExec>("run") {
             setArgsString(version)
