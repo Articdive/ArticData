ec67199407f7dd85383e0252a2b77156518635f9
Articdive <articdata@bot.io>
2022-05-06 16:35:11 +0000

    Update from https://github.com/Articdive/ArticDataGenerator/commit/25e366e2ccb4198f2e02911a091e49ea1f123a31

--- .github/README.md
+++ .github/README.md
@@ -56,7 +56,7 @@ Adding to a Maven Project:
     <dependency>
         <groupId>de.articdive</groupId>
         <artifactId>articdata</artifactId>
-        <version>1.18.1-f2691f32</version>
+        <version>1.18.1-25e366e2</version>
     </dependency>
 </dependencies>
 ```
@@ -68,7 +68,7 @@ repositories {
     mavenCentral()
 }
 dependencies {
-    implementation("de.articdive:articdata:1.18.1-f2691f32")
+    implementation("de.articdive:articdata:1.18.1-25e366e2")
 }
 ```
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 tasks {
     register<Jar>("packageVersion") {
         archiveBaseName.set("articdata")
-        archiveVersion.set("1.18.1-f2691f32")
+        archiveVersion.set("1.18.1-25e366e2")
 
         destinationDirectory.set(layout.buildDirectory.dir("dist"))
         from(project.rootProject.rootDir)
@@ -20,7 +20,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "de.articdive"
             artifactId = "articdata"
-            version = "1.18.1-f2691f32"
+            version = "1.18.1-25e366e2"
 
             pom {
                 name.set("ArticData")
