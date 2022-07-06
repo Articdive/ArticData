995b43821781f5f3b259880014696a8febf75afc
Articdive <articdata@bot.io>
2022-07-06 18:15:54 +0000

    Update from https://github.com/Articdive/ArticDataGenerator/commit/70d75256dc1f0d19ec66a7e285f887d27779465d

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 tasks {
     register<Jar>("packageVersion") {
         archiveBaseName.set("articdata")
-        archiveVersion.set("1.18-88313627")
+        archiveVersion.set("1.18-70d75256")
 
         destinationDirectory.set(layout.buildDirectory.dir("dist"))
         from(project.rootProject.rootDir)
@@ -20,7 +20,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "de.articdive"
             artifactId = "articdata"
-            version = "1.18-88313627"
+            version = "1.18-70d75256"
 
             pom {
                 name.set("ArticData")
